7258094db2e3bcc8cec6d071d84ee499172c80c6
ayan4m1 <andrew@bulletlogic.com>
2022-04-05 23:36:18 -0400

    fix server side builds where no window exists

--- src/utils/units.js
+++ src/utils/units.js
@@ -1,9 +1,9 @@
 import { useMemo } from 'react';
 
-export const pxToMm = (pixels, ppi = window.devicePixelRatio * 96) =>
+export const pxToMm = (pixels, ppi = (window?.devicePixelRatio || 1) * 96) =>
   pixels * (25.4 / ppi);
 
-export const mmToPx = (mm, ppi = window.devicePixelRatio * 96) =>
+export const mmToPx = (mm, ppi = (window?.devicePixelRatio || 1) * 96) =>
   mm * (ppi / 25.4);
 
 export const useTextWidth = (text) =>
